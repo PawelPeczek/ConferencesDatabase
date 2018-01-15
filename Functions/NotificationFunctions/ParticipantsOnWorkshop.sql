ae3eacd4cef80cad5096f7af2bd1a8b02987febe
renderowy <pawel.m.peczek@gmail.com>
2018-01-15 23:57:35 +0100

    Some bux with function ParticipantsOnWOrkshop fixed.

--- Functions/NotificationFunctions/ParticipantsOnWorkshop.sql
+++ Functions/NotificationFunctions/ParticipantsOnWorkshop.sql
@@ -1,16 +1,23 @@
 CREATE FUNCTION ParticipantsOnWorkshop(@WorkshopID INT)
 RETURNS @res TABLE (
+  ParticipantID INT,
   ParticipantName VARCHAR(32),
-  ParticipantSurname VARCHAR(32)
+  ParticipantSurname VARCHAR(32),
+  ClientID INT,
+  Client VARCHAR (64)
 )
 AS
 BEGIN
-    INSERT INTO @res(ParticipantName,ParticipantSurname)
-    SELECT p.Name,p.Surname
-    FROM Participants p 
-	JOIN ParticipAtConfDay pacd ON pacd.ParticipantID = p.ParticipantID
-	JOIN ParticipantsAtWorkshops paw ON paw.EntryID = pacd.EntryID
-	JOIN WorkshopsSubOrders wso On wso.WorkshopSubOrderID=paw.WorkshopSubOrderID
-    WHERE wso.WorkshopID = @WorkshopID
+  INSERT INTO @res(ParticipantId, ParticipantName, ParticipantSurname, ClientID, Client)
+    SELECT p.ParticipantID, p.Name, p.Surname, bci.ClientID, bci.ClientName
+      FROM Participants p
+      JOIN ParticipAtConfDay pacd ON p.ParticipantID = pacd.ParticipantID
+      JOIN ParticipantsAtWorkshops paw ON pacd.EntryID = paw.EntryID
+      JOIN WorkshopsSubOrders wso ON paw.WorkshopSubOrderID = wso.WorkshopSubOrderID
+      JOIN Workshops w ON wso.WorkshopID = w.WorkshopID
+      JOIN OrdersOnConfDays oocd ON pacd.OrdOnConfDayID = oocd.OrdOnConfDayID
+      JOIN Orders o ON oocd.OrderID = o.OrderID
+      JOIN BasicClientsInfo bci ON bci.ClientID = o.ClientID
+      WHERE w.WorkshopID = 9364
   RETURN
 END
--- Views/BasicClientsInfo.sql
+++ Views/BasicClientsInfo.sql
@@ -1,9 +1,9 @@
 CREATE VIEW BasicClientsInfo
 AS
-SELECT c.ClientID, CONCAT(p.Name, ' ', p.Surname)'Contact Person', c.PhoneNumber
+SELECT c.ClientID, CONCAT(p.Name, ' ', p.Surname)'Contact Person', c.PhoneNumber, 'Individual client' [ClientName]
 FROM Clients c JOIN IndividualsDetails id ON c.ClientID = id.ClientID
 JOIN Participants p ON id.ParticipantID = p.ParticipantID
 UNION
-SELECT c.ClientID, CONCAT(cd.ContactPersonName, ' ', cd.ContactPersonSurname)'Contact Person', c.PhoneNumber
+SELECT c.ClientID, CONCAT(cd.ContactPersonName, ' ', cd.ContactPersonSurname)'Contact Person', c.PhoneNumber, cd.CompanyName
 FROM Clients c JOIN CompDetails cd ON c.ClientID = cd.ClientID
 JOIN Orders o ON c.ClientID = o.ClientID
