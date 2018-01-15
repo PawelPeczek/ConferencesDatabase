5ca83a5fa9c6352a46b60a9512f1241cd993fb2a
renderowy <pawel.m.peczek@gmail.com>
2018-01-15 18:49:12 +0100

    ClientWhoPaidMost view fix.

--- Views/ClientWhoPaidMostView.sql
+++ Views/ClientWhoPaidMostView.sql
@@ -1,22 +1,14 @@
+DROP VIEW ClientWhoPaidMostView
 CREATE VIEW ClientWhoPaidMostView
 AS
-SELECT t1.ClientID AS ' Who', CAST(SUM([PaidForWorkshops]+[PaidForConferences]) AS DECIMAL (15,2)) AS 'Paid'
-FROM (SELECT o.ClientID,w.Name, SUM(wso.NumberOfSeats*w.Value) AS 'PaidForWorkshops'
-FROM Orders o JOIN OrdersOnConfDays oocd ON o.OrderID =oocd.OrderID
-	JOIN WorkshopsSubOrders wso ON wso.OrdOnConfDayID = oocd.OrdOnConfDayID
-	JOIN DaysOfConf doc ON doc.DayOfConfID =oocd.DayOfConfID
-	JOIN Workshops w ON w.WorkshopID = wso.WorkshopID
-WHERE o.Status=0
-GROUP BY o.ClientID, w.Name) AS t1
-JOIN
-(SELECT con.ConfName ,o.ClientID, 
-SUM(pt.Value*(oocd.NumberOfRegularSeats + oocd.NumberOfStudentSeats*(1-con.StudentDiscount))) AS 'PaidForConferences'
-FROM Conferences con
-JOIN DaysOfConf doc ON con.ConfID = doc.ConfID
-JOIN PriceThresholds pt ON pt.DayOfConfID = doc.DayOfConfID
-JOIN OrdersOnConfDays oocd ON oocd.DayOfConfID = doc.DayOfConfID
-JOIN Orders o ON o.OrderID = oocd.OrderID
-WHERE o.Status=0
-GROUP BY o.ClientID, con.ConfName) t2
-ON t1.ClientID = t2.ClientID
-GROUP BY t1.ClientID
+SELECT c.ClientID, CONCAT(p.Name, ' ', p.Surname)'Contact Person', c.PhoneNumber, SUM(t.Total) [Total] FROM Clients c JOIN IndividualsDetails id ON c.ClientID = id.ClientID
+JOIN Participants p ON id.ParticipantID = p.ParticipantID
+JOIN Orders o ON c.ClientID = o.ClientID
+JOIN GetValueOfAllOrders() t ON t.OrderId = o.OrderID
+GROUP BY c.ClientID, CONCAT(p.Name, ' ', p.Surname), c.PhoneNumber
+UNION
+SELECT c.ClientID, CONCAT(cd.ContactPersonName, ' ', cd.ContactPersonSurname)'Contact Person', c.PhoneNumber,
+        SUM(t.Total) [Total] FROM Clients c JOIN CompDetails cd ON c.ClientID = cd.ClientID
+JOIN Orders o ON c.ClientID = o.ClientID
+JOIN GetValueOfAllOrders() t ON t.OrderId = o.OrderID
+GROUP BY c.ClientID, cd.ContactPersonName, cd.ContactPersonSurname, c.PhoneNumber
